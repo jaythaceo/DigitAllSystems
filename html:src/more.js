f9ee0ddce43278e53fb807c092c92486ce1fcc99
jaythaceo <jaythaceo@gmail.com>
2014-02-04 17:12:46 -0500

    Added new files around the board a little messy will clean up later

---
+++ html:src/more.js
@@ -0,0 +1,11 @@
+/* Javascript controls for 
+ * Front.html
+ */
+
+ /******************
+ * By: Jason Brooks
+ * jaythaceo@gmail.com
+ *******************/
+
+ var jason = "jason";
+ document.write(jason);
