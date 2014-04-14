a9c99789d6055341028bd6c852dc47c98105b674
jaythaceo <jaythaceo@gmail.com>
2014-04-14 15:30:49 -0400

    added php file

---
+++ html:src/LandingPage/landing-page/logging.php
@@ -0,0 +1,32 @@
+
+
+<?php
+
+/ location of the text file that will log all the ip adresses
+$file = 'logfile.txt';
+
+// ip address of the visitor
+$ipadress = $_SERVER['REMOTE_ADDR'];
+
+// date of the visit that will be formated this way: 29/May/2011 12:20:03
+$date = date('d/F/Y h:i:s');
+
+// name of the page that was visited
+$webpage = $_SERVER['SCRIPT_NAME'];
+
+// visitor's browser information
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+// Opening the text file and writing the visitor's data
+$fp = fopen($file, 'a');
+
+fwrite($fp, $ipadress.' - ['.$date.'] '.$webpage.' '.$browser."\r\n");
+
+fclose($fp);
+
+
+
+
+
+
+?>
